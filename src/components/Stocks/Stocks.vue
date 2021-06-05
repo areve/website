0ab4570f126920350a8ee18adab19f4c505bf44a
areve <areve@challen.info>
2021-06-05 18:42:47 +0100

    more stocks work

--- src/components/Stocks/Stocks.vue
+++ src/components/Stocks/Stocks.vue
@@ -1,9 +1,31 @@
 <template>
   <article>
     <h1>Stocks</h1>
-    <p>I'm going to see if I can show stock prices on a page.</p>
-    <pre>{{ tickers }}</pre>
-    <pre>{{ data }}</pre>
+    <p>
+      This is experiment of showing some stock prices, and updating
+      continuously. It uses unsupported ways of getting data from yahoo, so it's
+      good for experimental purposes only. And it relies on a tiny Azure
+      Function I wrote, because of CORS issues with scraping yahoo's page.
+    </p>
+
+    <div class="cards">
+      <div v-for="(item, key) in data" :key="key" class="card">
+        <h2>{{ item.id }}</h2>
+        <p>{{ item.shortName }} - {{ item.quoteType }}</p>
+        <dl>
+          <dt>Price</dt>
+          <dd>{{ item.price.toFixed(2) }} {{ item.currency }}</dd>
+        </dl>
+        <dl>
+          <dt>Day Range</dt>
+          <dd>{{ item.dayLow.toFixed(2) }} - {{ item.dayHigh.toFixed(2) }}</dd>
+        </dl>
+        <dl>
+          <dt>Time</dt>
+          <dd>{{ new Date(item.time).toLocaleString() }}</dd>
+        </dl>
+      </div>
+    </div>
   </article>
 </template>
 
@@ -14,20 +36,37 @@ import * as StockSocket from "stocksocket";
 import { Buffer } from "buffer";
 (window as any).Buffer = Buffer;
 
+import * as yahooStockPrices from "./lib/yahoo-stock-prices";
+
 export default defineComponent({
   name: "Stocks",
   setup: () => ({
-    tickers: ref(["BTC-USD", "DOGE-USD"]),
+    tickers: ref(["BTC-USD", "DOGE-USD", "GME"]),
     data: ref({} as { [name: string]: any }),
   }),
-  mounted() {
+  async mounted() {
     const stockPriceChanged = (data: any) => {
       this.data[data.id] = data;
     };
+
+    this.tickers.forEach(async (ticker) => {
+      const data = await yahooStockPrices.getCurrentData(ticker);
+      this.data[data.id] = data;
+    });
+
     StockSocket.addTickers(this.tickers, stockPriceChanged);
   },
 });
 </script>
 
 <style scoped>
+.cards {
+  display: flex;
+  flex-flow: row wrap;
+  margin: -0.5em;
+}
+.card {
+  min-width: 10em;
+  margin: 0.5em;
+}
 </style>
---
+++ src/components/Stocks/lib/yahoo-stock-prices.ts
@@ -0,0 +1,84 @@
+import axios from "axios";
+
+var baseUrl =
+  "https://func-stocks-areve.azurewebsites.net/api/yahoo-finance-proxy?";
+
+function escapeRegExp(value: string) {
+  return value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+export var getCurrentData = function (ticker: string): any {
+  return new Promise(async function (resolve, reject) {
+    const response = await axios.get(baseUrl + ticker + "/");
+    var dataJson = response.data.match(
+      new RegExp(
+        '"' +
+          escapeRegExp(ticker) +
+          '":({"sourceInterval".*?),"[^"]*?":{"sourceInterval":'
+      )
+    );
+
+    const data = JSON.parse(dataJson[1]);
+
+    // object attempts to match format from "stocksocket"
+    resolve({
+      id: ticker,
+      price: data.regularMarketPrice.raw,
+      time: new Date().getTime(),
+      currency: data.currency,
+      exchange: data.exchange,
+      quoteType: data.quoteType,
+      marketHours: null,
+      changePercent: 0,
+      dayVolume: data.regularMarketVolume.raw,
+      dayHigh: data.regularMarketDayHigh.raw,
+      dayLow: data.regularMarketDayLow.raw,
+      change: 0,
+      shortName: data.shortName,
+      lastSize: null,
+      priceHint: data.priceHint,
+      vol_24hr: null,
+      volAllCurrencies: null,
+      fromcurrency: data.fromCurrency,
+      lastMarket: data.quoteSourceName,
+      circulatingSupply: null,
+      marketcap: data.marketCap.raw,
+    });
+  });
+};
+
+// export var getHistoricalPrices = function (startMonth, startDay, startYear, endMonth, endDay, endYear, ticker, frequency, callback, cors) {
+//   if (callback === void 0) { callback = undefined; }
+//   if (cors === void 0) { cors = "no-cors"; }
+//   var startDate = Math.floor(Date.UTC(startYear, startMonth, startDay, 0, 0, 0) / 1000);
+//   var endDate = Math.floor(Date.UTC(endYear, endMonth, endDay, 0, 0, 0) / 1000);
+//   var promise = new Promise(function (resolve, reject) {
+//     var requestOptions = {
+//       method: "GET",
+//       mode: cors
+//     };
+//     fetch(baseUrl + ticker + "/history?period1=" + startDate + "&period2=" + endDate + "&interval=" + frequency + "&filter=history&frequency=" + frequency, requestOptions)
+//       .then(function (response) { return response.text(); })
+//       .then(function (body) {
+//         var prices = JSON.parse(body
+//           .split('HistoricalPriceStore":{"prices":')[1]
+//           .split(',"isPending')[0]);
+//         resolve(prices);
+//       })
+//       .catch(function (error) {
+//         console.log("error", error);
+//         reject(error);
+//         return;
+//       });
+//   });
+//   // If a callback function was supplied return the result to the callback.
+//   // Otherwise return a promise.
+//   if (typeof callback === "function") {
+//     promise
+//       .then(function (price) { return callback(null, price); })
+//       .catch(function (err) { return callback(err); });
+//   }
+//   else {
+//     return promise;
+//   }
+// };

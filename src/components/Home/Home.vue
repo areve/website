74d51b98bfc23871b30610aa2bc535fa862287b3
areve <areve@users.noreply.github.com>
2021-10-12 20:44:10 +0100

    change back

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -3,7 +3,7 @@
     <h1>challen.info</h1>
     <p>
       I'm making this site and I'm having a play with Vue3,
-      <em>(loving it)</em>. The site contains many experiments I've created
+      <em>(and loving it)</em>. The site contains many experiments I've created
       over a number of years, they were converted to typescript when they were
       added into this Vue3 site, as a result some of the code isn't the best it
       could be. I think in an Lean and Agile way, this means that each page is a

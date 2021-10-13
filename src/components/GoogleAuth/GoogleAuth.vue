f79cf77da7f28a54ea9ce46b8f75cb20a9c13509
areve <areve@users.noreply.github.com>
2021-10-13 17:49:06 +0100

    show profile of logged in user

--- src/components/GoogleAuth/GoogleAuth.vue
+++ src/components/GoogleAuth/GoogleAuth.vue
@@ -1,16 +1,38 @@
 <template>
   <h1>Google Auth</h1>
   <p>Example of using Google Auth.</p>
+  <article class="profile" v-if="profile?.id">
+    <img
+      class="profile-image"
+      referrerpolicy="no-referrer"
+      :src="profile.imageUrl"
+      width="50"
+      height="50"
+    />
+    <h2 class="profile-name">{{ profile.name }}</h2>
+    <p class="profile-email">{{ profile.email }}</p>
+  </article>
 
-  <button @click="signIn" :disabled="!isInit || isAuthorized">Sign In</button>
-  <button @click="signOut" :disabled="!isInit || !isAuthorized">
-    Sign Out
+  <button
+    type="button"
+    @click="signIn"
+    :disabled="!isInit"
+    v-if="!isAuthorized"
+  >
+    Sign-in
   </button>
-  <p>Username: {{ username }}</p>
-  <pre>
+  <button
+    type="button"
+    @click="signOut"
+    :disabled="!isInit"
+    v-if="isAuthorized"
+  >
+    Sign-out
+  </button>
+  <pre v-if="showDebug">
 isInit: {{ isInit }}
 isAuthorized: {{ isAuthorized }}
-googleUser: {{ googleUser }}
+profile: {{ profile }}
     </pre
   >
   <!-- TODO Remove code from main.ts if possible -->
@@ -19,6 +41,7 @@ googleUser: {{ googleUser }}
 <script lang="ts">
 import { defineComponent } from "vue";
 import gAuth from "vue3-google-auth";
+import { router } from "../../router";
 
 export default defineComponent({
   name: "GoogleAuth",
@@ -29,36 +52,77 @@ export default defineComponent({
       this.isInit = !!$gAuth.isInit;
       this.isAuthorized = !!$gAuth.isAuthorized;
       this.$gAuth = $gAuth;
-      this.googleUser = this.$gAuth?.GoogleAuth?.currentUser?.Xd;
-      this.username = this.googleUser?.dt?.Se
-      if (this.isInit) clearInterval(checkGauthLoad);
-    }, 100);
+      this.profile = this.googleUserToProfile(
+        this.$gAuth?.GoogleAuth?.currentUser?.get()
+      );
+      if (this.isInit) {
+        clearInterval(checkGauthLoad);
+        router.replace({ path: "/google-auth" });
+      }
+    }, 200);
   },
   data: () => {
     return {
       isInit: false,
       isAuthorized: false,
+      showDebug: false,
       $gAuth: null as any,
-      googleUser: null as any,
-      username: null as any
+      profile: {} as any,
     };
   },
   methods: {
+    googleUserToProfile(googleUser: any) {
+      const profile = googleUser?.getBasicProfile();
+      return (
+        (profile && {
+          id: profile.getId(),
+          name: profile.getName(),
+          givenName: profile.getGivenName(),
+          familyName: profile.getFamilyName(),
+          imageUrl: profile.getImageUrl(),
+          email: profile.getEmail(),
+        }) ||
+        {}
+      );
+    },
     async signIn() {
       const googleUser = await this.$gAuth.signIn();
       this.isAuthorized = this.$gAuth.isAuthorized;
-      this.googleUser = googleUser;
-      this.username = this.googleUser?.dt?.Se
+      this.profile = this.googleUserToProfile(googleUser);
     },
     async signOut() {
       await this.$gAuth.signOut();
       this.isAuthorized = this.$gAuth.isAuthorized;
-      this.googleUser = null;
-      this.username = null;
+      this.profile = {};
     },
   },
 });
 </script>
 
 <style scoped>
+.profile {
+  padding: 2em 0 1em 0;
+  display: grid;
+  grid-template-columns: calc(50px + 1em) 1fr;
+  width: fit-content;
+}
+
+.profile-image {
+  grid-column: 1 / 1;
+  grid-row: 1 / span 2;
+  border-radius: 50%;
+}
+
+.profile-name {
+  font-size: 1.2em;
+  font-weight: normal;
+  margin: 0 0 0.3em 0;
+  grid-column: 2 / 2;
+  grid-row: 1 / 2;
+}
+.profile-email {
+  margin: 0;
+  grid-column: 2 /2;
+  grid-row: 2 / 2;
+}
 </style>

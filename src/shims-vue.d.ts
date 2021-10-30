0d985832de5af9cb537f76b4aa46aef8b73ddb27
areve <areve@users.noreply.github.com>
2021-10-30 17:38:10 +0100

    split shims

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,18 +1,16 @@
-// declare module '*.vue' {
-//   import { DefineComponent } from 'vue'
-//   const component: DefineComponent<{}, {}, any>
-//   export default component
-// }
-
-
-
- import { ComponentCustomProperties } from 'vue'
- import { Store } from 'vuex'
-import { RootState } from './store'
-
-declare module '@vue/runtime-core' {
-
-  interface ComponentCustomProperties {
-    $store: Store<RootState>
-  }
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
+
+//  import { ComponentCustomProperties } from 'vue'
+//  import { Store } from 'vuex'
+// import { RootState } from './store'
+
+// declare module '@vue/runtime-core' {
+
+//   interface ComponentCustomProperties {
+//     $store: Store<RootState>
+//   }
+// }

c1a1be524b0a5fcdd3064380f736fe50e55cd4f8
areve <areve@users.noreply.github.com>
2021-10-30 17:38:21 +0100

    remove comment

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,14 +3,3 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-
-//  import { ComponentCustomProperties } from 'vue'
-//  import { Store } from 'vuex'
-// import { RootState } from './store'
-
-// declare module '@vue/runtime-core' {
-
-//   interface ComponentCustomProperties {
-//     $store: Store<RootState>
-//   }
-// }

38ac73524b1befb64c6587d973b7f1a80ed9a9ec
CI <areve>
2021-11-02 09:01:57 +0000

    deploy

---
+++ assets/FlatCss.bc976c5e.js
@@ -0,0 +1,4 @@
+import{_ as e}from"./index.9a7d996a.js";import{d as a,o,c as n,e as s,f as t}from"./vendor.32b706e2.js";const c=a({name:"FlatCss",setup:()=>({})}),l=s("h1",null,"flat.css",-1),r=t(),i=s("p",null,[t(`
+      This site uses `),s("strong",null,[s("a",{href:"https://areve.github.io/flatcss2/"},"flat.css")]),t(` which is my own css library.
+      It's basically a fancy css reset that allows all the html5 tags to look nice without adding any class attributes.
+    `)],-1),d=[l,r,i];function _(h,f,p,u,m,b){return o(),n("article",null,d)}var g=e(c,[["render",_]]);export{g as default};
---
+++ assets/Seaside.7f4214ed.js
@@ -0,0 +1,4 @@
+import{_ as o}from"./index.9a7d996a.js";import{d as n,o as d,c as _,p as c,l as r,f as a,e as s}from"./vendor.32b706e2.js";var i="/assets/sand-castle.eb7e0067.jpg";const l=n({name:"Seaside",setup:()=>({})}),t=e=>(c("data-v-295e4687"),e=e(),r(),e),p=t(()=>s("h1",null,"Seaside",-1)),u=a(),h=t(()=>s("p",null,`
+      Here is a sand castle that the kids and I made, our castles are not like
+      everyone else\u2019s.
+    `,-1)),m=a(),f=t(()=>s("img",{class:"sand-castle-photo",alt:"Sand Castle Photo",src:i},null,-1)),v=[p,u,h,m,f];function S(e,x,I,k,$,g){return d(),_("article",null,v)}var B=o(l,[["render",S],["__scopeId","data-v-295e4687"]]);export{B as default};

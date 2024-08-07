c287c738017667d35c4f424a8b62430c996b2630
areve <areve@challen.info>
2024-08-07 20:42:39 +0100

    add a galaxy

--- src/components/World/World.vue
+++ src/components/World/World.vue
@@ -4,38 +4,64 @@
     <p>A psuedo random number generated world map</p>
     <p>click the top map to see the detail</p>
 
-    <div class="canvas-wrap">
-      <canvas
-        ref="universeCanvas"
-        class="canvas"
-        @click="clickUniverse"
-        @mousemove="hoverUniverse"
-      ></canvas>
-    </div>
-    <div>{{ Math.floor(hover) }}</div>
-    <div>{{ Math.floor(clickData) }}</div>
-    <div class="canvas-wrap">
-      <canvas ref="planetCanvas" class="canvas"></canvas>
-    </div>
+    <section class="group">
+      <div class="canvas-wrap">
+        <canvas
+          ref="universeCanvas"
+          class="canvas"
+          @click="clickUniverse"
+          @mousemove="hoverUniverse"
+        ></canvas>
+      </div>
+      <div class="notes">
+        <div>{{ Math.floor(universeHover) }}</div>
+      </div>
+    </section>
+    <section class="group">
+      <div class="canvas-wrap">
+        <canvas ref="galaxyCanvas" class="canvas"></canvas>
+      </div>
+      <div class="notes">
+        <div>{{ Math.floor(galaxyData) }}</div>
+      </div>
+    </section>
+    <section class="group">
+      <div class="canvas-wrap">
+        <canvas ref="planetCanvas" class="canvas"></canvas>
+      </div>
+      <div class="notes">
+        <div>{{ Math.floor(planetData) }}</div>
+      </div>
+    </section>
   </section>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, Ref, ref } from "vue";
-import { makeUniverseMap, renderUniverse, UniverseMapData } from "./maps/universeMap";
+import {
+  makeUniverseMap,
+  renderUniverse,
+  UniverseMapData,
+} from "./maps/universeMap";
 import { makePlanetMap, PlanetMapData, renderPlanet } from "./maps/planetMap";
 import { MapData, xor } from "./lib/other";
+import { GalaxyMapData, makeGalaxyMap, renderGalaxy } from "./maps/galaxyMap";
 
 const universeCanvas = ref<HTMLCanvasElement>(undefined!);
 let universeContext: CanvasRenderingContext2D | null;
 let universeMap: UniverseMapData;
 
+const galaxyCanvas = ref<HTMLCanvasElement>(undefined!);
+let galaxyContext: CanvasRenderingContext2D | null;
+let galaxyMap: GalaxyMapData;
+
 const planetCanvas = ref<HTMLCanvasElement>(undefined!);
 let planetContext: CanvasRenderingContext2D | null;
 let planetMap: PlanetMapData;
 
-const hover = ref(0.45);
-const clickData = ref(0.45);
+const universeHover = ref(0);
+const planetData = ref(0);
+const galaxyData = ref(0);
 
 const seed = new TextEncoder().encode("This is the seed");
 
@@ -44,11 +70,14 @@ const height = 256;
 
 onMounted(async () => {
   universeContext = getContext(universeCanvas, width, height);
+  galaxyContext = getContext(galaxyCanvas, width, height);
   planetContext = getContext(planetCanvas, width, height);
 
   const universeWeightKg = 1e53; // TODO will use this again later
   universeMap = makeUniverseMap(width, height, seed, universeWeightKg);
   renderUniverse(universeContext, universeMap);
+
+  updateGalaxy(0);
 
   updatePlanet(0);
 
@@ -70,7 +99,7 @@ const hoverUniverse = (event: MouseEvent) => {
     y: event.offsetY,
   };
   const coord = point.y * width + point.x;
-  hover.value = (universeMap.weights[coord] / 0xffffffff) * 255;
+  universeHover.value = (universeMap.weights[coord] / 0xffffffff) * 255;
 };
 
 const clickUniverse = (event: MouseEvent) => {
@@ -79,10 +108,21 @@ const clickUniverse = (event: MouseEvent) => {
     y: event.offsetY,
   };
   const coord = point.y * width + point.x;
-  clickData.value = (universeMap.weights[coord] / 0xffffffff) * 255;
+  planetData.value = (universeMap.weights[coord] / 0xffffffff) * 255;
+  galaxyData.value = (universeMap.weights[coord] / 0xffffffff) * 255;
   updatePlanet(coord);
+  updateGalaxy(coord);
 };
 
+function updateGalaxy(coord: number) {
+  galaxyMap = makeGalaxyMap(
+    width,
+    height,
+    xor(universeMap.state, universeMap.states[coord]),
+    universeMap.weights[coord]
+  );
+  renderGalaxy(galaxyContext, galaxyMap);
+}
 function updatePlanet(coord: number) {
   planetMap = makePlanetMap(
     width,
@@ -108,6 +148,17 @@ function getContext(
 </script>
 
 <style scoped>
+.group {
+  display: flex;
+  margin-bottom: 5px;
+}
+.notes {
+  flex: 1 1;
+  /* border: 1px solid red; */
+  background-color: #eee;
+  margin-left: 5px;
+  padding: 5px;
+}
 .canvas-wrap {
   position: relative;
   height: 256px;
---
+++ src/components/World/maps/galaxyMap.ts
@@ -0,0 +1,55 @@
+import { MapData, makeMap } from "../lib/other";
+
+export interface GalaxyMapData extends MapData {
+  weight: number;
+  weights: Uint32Array;
+}
+
+export function makeGalaxyMap(
+  width: number,
+  height: number,
+  seed: Uint8Array,
+  weight: number
+) {
+  let map = makeMap(width, height, seed);
+  const weights = new Uint32Array(
+    map.states.map(
+      (v) => ((v[0] << 24) | (v[1] << 16) | (v[2] << 8) | v[3]) >>> 0
+    )
+  );
+  let galaxy: GalaxyMapData = {
+    ...map,
+    weights,
+    weight,
+  };
+
+  return galaxy;
+}
+
+export function renderGalaxy(
+  context: CanvasRenderingContext2D | null,
+  map: GalaxyMapData
+) {
+  if (!context) return;
+
+  const width = context.canvas.width;
+  const height = context.canvas.height;
+  context.clearRect(0, 0, width, height);
+
+  const data = map.weights;
+  const imageData = new ImageData(width, height);
+  const pixelData = imageData.data;
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const i = y * width + x;
+      const o = (y * width + x) * 4;
+      pixelData[o + 0] = ((data[i] / 0xffffffff) * 255) & 0xff;
+      pixelData[o + 1] = ((data[i] / 0xffffffff) * 255) & 0xff; //(data[i] >> 8) & 0xff;
+      pixelData[o + 2] = ((data[i] / 0xffffffff) * 255) & 0xff; //(data[i] >> 16) & 0xff;
+
+      pixelData[o + 3] = 255;
+    }
+  }
+
+  context.putImageData(imageData, 0, 0);
+}

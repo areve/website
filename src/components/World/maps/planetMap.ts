1fb7ee2e5a98dbe15a66875cf0db966917a78d2d
areve <areve@challen.info>
2024-08-10 16:41:26 +0100

    refactor

--- src/components/World/maps/planetMap.ts
+++ src/components/World/maps/planetMap.ts
@@ -1,3 +1,4 @@
+import { extend } from "hammerjs";
 import { diskFilter } from "../filters/diskFilter";
 import { applyFilter, seedToFloat, stretchContrast } from "../lib/other";
 import { LayerProps, Layer, getStates } from "../lib/prng";
@@ -17,11 +18,10 @@ export function makePlanetLayer(props: PlanetProps) {
   const states = getStates(props);
   const floats = states.map(seedToFloat);
   const filter = diskFilter(10);
-  const blurred = applyFilter(floats, props.width, filter)
-  const heights = stretchContrast(blurred)
+  const blurred = applyFilter(floats, props.width, filter);
+  const heights = stretchContrast(blurred);
   return { states, props, heights } as PlanetLayer;
 }
-
 
 export const getPlanetPixels = (layer: PlanetLayer) =>
   layer.heights.map((v) => {

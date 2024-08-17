f0efa1a21e6ec209470c4666f9eaef58cd70aec4
areve <areve@challen.info>
2024-08-17 20:53:48 +0100

    looking good

--- src/components/World/PlanetLayer.vue
+++ src/components/World/PlanetLayer.vue
@@ -161,11 +161,24 @@ const sizes = (coord: Coord) => {
   return generator(coord) * scale;
 };
 
-const temperature = (coord: Coord) => bicubic(coord, 1 / 43, generator);
-const moisture = (coord: Coord) =>
-  bicubic(coord, 1 / 67, (coord: Coord) =>
-    generator({ x: coord.x - 127.5, y: coord.y - 127.5 })
-  );
+const temperature = (coord: Coord) => {
+  const a = bicubic(coord, 1 / 65, generator);
+  const b = bicubic(coord, 1 / 33, generator);
+  const c = bicubic(coord, 1 / 15, generator);
+  const d = bicubic(coord, 1 / 9, generator);
+
+  return a * 0.5 + b * 0.3 + c * 0.2 + d * 0.1;
+};
+
+const moisture = (coord: Coord) => {
+  const moistCoord = { x: coord.x - 127.5, y: coord.y - 127.5 }
+  const a = bicubic(moistCoord, 1 / 65, generator);
+  const b = bicubic(moistCoord, 1 / 33, generator);
+  const c = bicubic(moistCoord, 1 / 15, generator);
+  const d = bicubic(moistCoord, 1 / 9, generator);
+
+  return a * 0.25 + b * 0.25 + c * 0.25 + d * 0.25;
+};
 
 const heightFilterRadius = 20;
 const heightFilter = diskFilter(heightFilterRadius);
@@ -185,7 +198,7 @@ function heights_faster(coord: Coord) {
 
   const d = generator({ x: coord.x - 1.5, y: coord.y - 1.5 });
 
-  return a * 0.3 + b * 0.52 + c * 0.2 + d * 0.08;
+  return a * 0.35 + b * 0.47 + c * 0.2 + d * 0.08;
 }
 function heights_old_slow(coord: Coord) {
   // TODO this applyFilter is really good but so slow
@@ -209,9 +222,10 @@ function pixel(coord: Coord) {
   const t = c(temperature(coord));
   const m = c(moisture(coord));
 
-  const isSea = h < 0.6;
-  const sd = (1 - h / 0.6) ** 0.5;
-  const sh = (h - 0.6) / 0.4;
+  const seaLevel = 0.6
+  const isSea = h < seaLevel;
+  const sd = (1 - h / seaLevel) ** 0.2;
+  const sh = ((h - seaLevel) / (1 - seaLevel)) ** 0.5;
   const i = c(heightIcinessCurve(h) + temperatureIcinessCurve(t));
 
   if (isSea) {
@@ -226,26 +240,26 @@ function pixel(coord: Coord) {
     ];
     return hsv2rgb([
       seaHsv[0], //
-      c(seaHsv[1] - 0.5 * i),
-      c(seaHsv[2] + 0.2 * i),
+      c(seaHsv[1] - 0.3 * i),
+      c(seaHsv[2] + 0.6 * i),
     ]);
   } else {
     // jungle hsv(92, 41%, 23%)
     // tundra hsv(78, 24%, 27%)
-    // desert hsv(37, 35%, 89%) 
+    // desert hsv(37, 35%, 89%)
     // australia hsv(31, 41%, 58%)
     // grass hsv(77, 34%, 40%)
     // mountain hsv(35, 21%, 64%)
     const landHsv: Hsv = [
       //
       77 / 360 - sh * (32 / 360) - 16 / 360 + m * (50 / 360),
-      0.34 - sh * 0.13 + (1-m) * .05 + 0.1 - (1-t) * 0.2,
-      0.4 - sh * 0.24 + (1-m) * .3 - (1-t) * 0.1,
+      0.34 - sh * 0.13 + (1 - m) * 0.05 + 0.1 - (1 - t) * 0.2,
+      0.4 - sh * 0.24 + (1 - m) * 0.3 - (1 - t) * 0.1,
     ]; //[0.05 + m ** 0.6 * 0.2, 0.8 - h * 0.2, 1.4 - h];
     return hsv2rgb([
       landHsv[0],
-      c(landHsv[1] - 0.8 * i),
-      c(landHsv[2] + 0.5 * i),
+      c(landHsv[1] - 0.3 * i),
+      c(landHsv[2] + 0.6 * i),
     ]);
   }
 }

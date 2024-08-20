9dd5bab6e541101798275ca741234db429db7b5a
areve <areve@challen.info>
2024-08-20 20:38:57 +0100

    OpenSimplexNoise

--- src/components/World/Universe2Layer.vue
+++ src/components/World/Universe2Layer.vue
@@ -23,7 +23,7 @@
 import { onMounted, ref, watch } from "vue";
 import { Coord, Dimensions } from "./lib/interfaces";
 import { coordFromEvent, render } from "./lib/render";
-import { makePerlinNoiseGenerator } from "./noise/perlin";
+import { makeOpenSimplexNoiseGenerator } from "./noise/openSimplex";
 
 export interface UniverseProps {
   seed: number;
@@ -83,7 +83,7 @@ const click = (event: MouseEvent) => {
 };
 
 const update = () => {
-  generator = makePerlinNoiseGenerator(props.seed);
+  generator = makeOpenSimplexNoiseGenerator(props.seed);
   render(canvas.value, props.dimensions, pixel);
   selectionChanged({ x: 0, y: 0 });
 };

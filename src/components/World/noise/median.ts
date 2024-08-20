1dc4b31aa797455e42301bc05a143e7d9cad033e
areve <areve@challen.info>
2024-08-20 23:13:32 +0100

    added k

--- src/components/World/noise/median.ts
+++ src/components/World/noise/median.ts
@@ -9,18 +9,19 @@ export const makeMedianGenerator = (seed: number) => {
     const x = coord.x;
     const y = coord.y;
 
+    const k = 1 // 1 is correct but 2 looks interesting
     // Get noise values at the 3x3 grid around (x, y)
-    const p0 = noise({ x: x - 1, y: y - 1 });
-    const p1 = noise({ x, y: y - 1 });
-    const p2 = noise({ x: x + 1, y: y - 1 });
+    const p0 = noise({ x: x - k, y: y - k });
+    const p1 = noise({ x, y: y - k });
+    const p2 = noise({ x: x + k, y: y - k });
 
-    const p3 = noise({ x: x - 1, y });
+    const p3 = noise({ x: x - k, y });
     const p4 = noise({ x, y });
-    const p5 = noise({ x: x + 1, y });
+    const p5 = noise({ x: x + k, y });
 
-    const p6 = noise({ x: x - 1, y: y + 1 });
-    const p7 = noise({ x, y: y + 1 });
-    const p8 = noise({ x: x + 1, y: y + 1 });
+    const p6 = noise({ x: x - k, y: y + k });
+    const p7 = noise({ x, y: y + k });
+    const p8 = noise({ x: x + k, y: y + k });
 
     // Calculate the median of the nine points
     const values = [p0, p1, p2, p3, p4, p5, p6, p7, p8].sort((a, b) => a - b);

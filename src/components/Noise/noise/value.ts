00e6ffbbe3f7230e0b112571ee94111b0e04cfe6
areve <areve@challen.info>
2024-08-24 09:36:58 +0100

    tidy

--- src/components/Noise/noise/value.ts
+++ src/components/Noise/noise/value.ts
@@ -1,20 +1,12 @@
 import { Coord } from "../lib/interfaces";
 import { makePointGenerator } from "./prng";
 
-function smootherstep(t: number): number {
-  return t * t * t * (t * (t * 6 - 15) + 10);
-}
-
-const smoothstep = (t: number): number => {
-  return t * t * (3 - t * 2);
-};
-
-const lerp = (a: number, b: number, t: number): number => {
-  return a + (b - a) * t;
-};
-
 export const makeValueNoiseGenerator = (seed: number) => {
   const noise = makePointGenerator(seed);
+
+  const smoothstep = (t: number): number => t * t * (3 - t * 2);
+
+  const lerp = (a: number, b: number, t: number): number => a + (b - a) * t;
 
   return (coord: Coord, scale: number = 8): number => {
     const x = Math.floor(coord.x / scale);

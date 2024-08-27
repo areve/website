a0535b2e5126485f8b4407a0652239f9a9dabfdd
areve <areve@challen.info>
2024-08-27 17:23:45 +0100

    tidy

--- src/components/Noise/noise/customSimplex.ts
+++ src/components/Noise/noise/customSimplex.ts
@@ -15,25 +15,29 @@ export const makeCustomSimplexGenerator = (seed: number) => {
     const skew = (x + y) * SKEW_2D;
     const xs = x + skew;
     const ys = y + skew;
-    const xsb = safeFloor(xs);
-    const ysb = safeFloor(ys);
+    const xsb = fastFloor(xs);
+    const ysb = fastFloor(ys);
     const xi = xs - xsb;
     const yi = ys - ysb;
 
     return (
-      vertexContribution(xsb, ysb, xi, yi) +
-      vertexContribution(xsb + 1, ysb + 1, xi - 1, yi - 1) +
-      vertexContribution(xsb + 1, ysb, xi - 1, yi) +
-      vertexContribution(xsb, ysb + 1, xi, yi - 1)
+      vertexContribution(xsb, ysb, xi, yi, 0, 0) +
+      vertexContribution(xsb, ysb, xi, yi, 1, 1) +
+      vertexContribution(xsb, ysb, xi, yi, 1, 0) +
+      vertexContribution(xsb, ysb, xi, yi, 0, 1)
     );
   }
 
   function vertexContribution(
     xsb: number,
     ysb: number,
-    dx: number,
-    dy: number
+    xi: number,
+    yi: number,
+    cx: number,
+    cy: number
   ): number {
+    const dx = xi - cx;
+    const dy = yi - cy;
     const skew = (dx + dy) * UNSKEW_2D;
     const dxs = dx + skew;
     const dys = dy + skew;
@@ -41,14 +45,14 @@ export const makeCustomSimplexGenerator = (seed: number) => {
     const a = 2 / 3 - dxs * dxs - dys * dys;
     if (a < 0) return 0;
 
-    const h = noise(xsb, ysb) * 0xff;
+    const h = noise(xsb + cx, ysb + cy) * 0xff;
     const u = (h & 0xf) - 8;
     const v = (h >> 4) - 8;
     const g = u * dxs + v * dys;
     return a * a * a * a * g;
   }
 
-  function safeFloor(x: number): number {
+  function fastFloor(x: number): number {
     const xi = x | 0;
     return x < xi ? xi - 1 : xi;
   }

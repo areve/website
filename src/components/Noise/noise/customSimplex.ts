bbd09a93a138e6d885d726e64c4db04d993c9ebd
areve <areve@challen.info>
2024-08-27 18:54:59 +0100

    tidy

--- src/components/Noise/noise/customSimplex.ts
+++ src/components/Noise/noise/customSimplex.ts
@@ -2,19 +2,18 @@ import { Coord } from "../lib/interfaces";
 import { makePointGeneratorFast } from "./prng";
 
 export const makeCustomSimplexGenerator = (seed: number) => {
-  const noise = makePointGeneratorFast(seed);
-
+  
   const skew2d = (Math.sqrt(3) - 1) / 2;
   const unskew2d = -(3 - Math.sqrt(3)) / 6;
+  const noise = makePointGeneratorFast(seed);
 
-  return (coord: Coord, scale: number): number => {
-    return noise2(coord.x / scale, coord.y / scale);
-  };
+  return (coord: Coord, scale: number): number =>
+    openSimplex(coord.x / scale, coord.y / scale);
 
-  function noise2(ix: number, iy: number): number {
+  function openSimplex(ix: number, iy: number): number {
     const skew = (ix + iy) * skew2d;
-    const x = fastFloor(ix + skew);
-    const y = fastFloor(iy + skew);
+    const x = Math.floor(ix + skew);
+    const y = Math.floor(iy + skew);
     const fx = ix + skew - x;
     const fy = iy + skew - y;
 
@@ -47,10 +46,5 @@ export const makeCustomSimplexGenerator = (seed: number) => {
     const u = (h & 0xf) - 8;
     const v = (h >> 4) - 8;
     return a * a * a * a * (u * dxs + v * dys);
-  }
-
-  function fastFloor(x: number): number {
-    const xi = x | 0;
-    return x < xi ? xi - 1 : xi;
   }
 };

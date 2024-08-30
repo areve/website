ec102c9bed42c82f7a8d6aefc3bf37d7554a85f0
areve <areve@challen.info>
2024-08-30 13:54:33 +0100

    more curves

--- src/components/Curves/Curves.vue
+++ src/components/Curves/Curves.vue
@@ -4,7 +4,7 @@
     <p>
       I needed a curve for a program to map some colours. This lead me to a
       whole lot of learning about curves and splines. These are some experiments
-      I created during that.
+      I created during that. And a tool to visualize and compare curves.
     </p>
 
     <CurvesGraph
@@ -23,9 +23,13 @@ import { getDevicePixelRatio } from "./lib/render";
 import { makeSmoothCurveFunction } from "./lib/curves";
 import CurvesGraph from "./CurvesGraph.vue";
 
+const smoothstepHalf = (t: number): number => (t * t * (3 - t * 2) + t) / 2;
+const smoothstep = (t: number): number => t * t * (3 - t * 2);
+const smootherstep = (t: number): number => t * t * t * (t * (t * 6 - 15) + 10);
+
 const funcs = [
   {
-    label: "a custom smooth curve",
+    label: "custom smooth curve",
     color: [1, 1, 0],
     func: makeSmoothCurveFunction([
       { x: 0, y: 0 },
@@ -34,6 +38,31 @@ const funcs = [
       { x: 0.85, y: 0.9 },
       { x: 1, y: 1 },
     ]),
+  },
+  {
+    label: "straight line",
+    color: [0, 1, 0],
+    func: (x: number) => x,
+  },
+  {
+    label: "sin based = (1 - cos(πx)) / 2",
+    color: [1, 0.2, 0.2],
+    func: (x: number) => (-Math.cos(Math.PI * x) + 1) / 2,
+  },
+  {
+    label: "smoothstep = t * t * t * (t * (t * 6 - 15) + 10)",
+    color: [0, 1, 1],
+    func: smoothstep,
+  },
+  {
+    label: "smootherstep = t * t * (3 - t * 2)",
+    color: [1, 0, 1],
+    func: smootherstep,
+  },
+  {
+    label: "smoothstepHalf - my own in-between variation",
+    color: [0.3, 0.5, 1],
+    func: smoothstepHalf,
   },
 ];
 </script>

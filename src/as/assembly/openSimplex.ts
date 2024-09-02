a696cc4f539829dd51d2da7f17880449cc274289
areve <areve@challen.info>
2024-09-02 21:26:07 +0100

    makeOpenSimplex3dGenerator in wasm

--- src/as/assembly/openSimplex.ts
+++ src/as/assembly/openSimplex.ts
@@ -3,6 +3,7 @@
 import { makeNoiseGenerator, NoiseGenerator } from "./prng";
 
 export type OpenSimplex2dGenerator = (x: f64, y: f64, z: f64) => f64;
+export type OpenSimplex3dGenerator = (x: f64, y: f64, z: f64) => f64;
 
 let noise!: NoiseGenerator;
 let _scale!: f64;
@@ -10,6 +11,10 @@ let _scale!: f64;
 const skew2d: f64 = (Math.sqrt(3) - 1) / 2;
 const unskew2d: f64 = -(3 - Math.sqrt(3)) / 6;
 const rSquared2d: f64 = 2 / 3;
+
+const skew3d: f64 = 1 / 3;
+const unskew3d: f64 = 1 / 6;
+const rSquared3d: f64 = 3 / 4;
 
 export const makeOpenSimplex2dGenerator = (
   seed: i64,
@@ -28,78 +33,16 @@ export const makeOpenSimplex2dGenerator = (
     const fy: f64 = ys + skew - iy;
 
     return (
-      vertexContribution(ix, iy, fx, fy, 0, 0) +
-      vertexContribution(ix, iy, fx, fy, 1, 0) +
-      vertexContribution(ix, iy, fx, fy, 0, 1) +
-      vertexContribution(ix, iy, fx, fy, 1, 1) +
+      vertexContribution2d(ix, iy, fx, fy, 0, 0) +
+      vertexContribution2d(ix, iy, fx, fy, 1, 0) +
+      vertexContribution2d(ix, iy, fx, fy, 0, 1) +
+      vertexContribution2d(ix, iy, fx, fy, 1, 1) +
       0.5
     );
   };
-
-  // };
-
-  // export const makeOpenSimplex3dGenerator = (seed: number, scale: number = 8) => {
-  //   const skew3d = 1 / 3;
-  //   const unskew3d = 1 / 6;
-  //   const rSquared3d = 3 / 4;
-  //   const noise = makeNoiseGenerator(seed);
-
-  //   return (x: number, y: number, z: number): number => {
-  //     const sx = x / scale;
-  //     const sy = y / scale;
-  //     const sz = z / scale;
-  //     const skew = (sx + sy + sz) * skew3d;
-  //     const ix = Math.floor(sx + skew);
-  //     const iy = Math.floor(sy + skew);
-  //     const iz = Math.floor(sz + skew);
-  //     const fx = sx + skew - ix;
-  //     const fy = sy + skew - iy;
-  //     const fz = sz + skew - iz;
-
-  //     return (
-  //       vertexContribution(ix, iy, iz, fx, fy, fz, 0, 0, 0) +
-  //       vertexContribution(ix, iy, iz, fx, fy, fz, 1, 0, 0) +
-  //       vertexContribution(ix, iy, iz, fx, fy, fz, 0, 1, 0) +
-  //       vertexContribution(ix, iy, iz, fx, fy, fz, 1, 1, 0) +
-  //       vertexContribution(ix, iy, iz, fx, fy, fz, 0, 0, 1) +
-  //       vertexContribution(ix, iy, iz, fx, fy, fz, 1, 0, 1) +
-  //       vertexContribution(ix, iy, iz, fx, fy, fz, 0, 1, 1) +
-  //       vertexContribution(ix, iy, iz, fx, fy, fz, 1, 1, 1) +
-  //       0.5
-  //     );
-  //   };
-
-  //   function vertexContribution(
-  //     ix: number,
-  //     iy: number,
-  //     iz: number,
-  //     fx: number,
-  //     fy: number,
-  //     fz: number,
-  //     cx: number,
-  //     cy: number,
-  //     cz: number
-  //   ): number {
-  //     const dx = fx - cx;
-  //     const dy = fy - cy;
-  //     const dz = fz - cz;
-  //     const skewedOffset = (dx + dy + dz) * unskew3d;
-  //     const dxs = dx - skewedOffset;
-  //     const dys = dy - skewedOffset;
-  //     const dzs = dz - skewedOffset;
-
-  //     const a = rSquared3d - dxs * dxs - dys * dys - dzs * dzs;
-  //     if (a < 0) return 0;
-
-  //     const h = noise(ix + cx, iy + cy, iz + cz) * 0xfff;
-  //     const u = (h & 0xf) - 8;
-  //     const v = ((h >> 4) & 0xf) - 8;
-  //     const w = ((h >> 8) & 0xf) - 8;
-  //     return (a * a * a * a * (u * dxs + v * dys + w * dzs)) / 2;
-  //   }
 };
 
-function vertexContribution(
+function vertexContribution2d(
   ix: f64,
   iy: f64,
   fx: f64,
@@ -120,4 +63,70 @@ function vertexContribution(
   const u = <f64>(<i64>(<i8>(h & 0xf) - 8));
   const v = <f64>(<i64>(<i8>(h >> 4) - 8));
   return a * a * a * a * (u * dxs + v * dys);
+}
+
+export const makeOpenSimplex3dGenerator = (
+  seed: i64,
+  scale: f64 = 8
+): OpenSimplex3dGenerator => {
+  noise = makeNoiseGenerator(seed);
+  _scale = scale;
+
+  return (x: f64, y: f64, z: f64): f64 => {
+    const sx: f64 = x / _scale;
+    const sy: f64 = y / _scale;
+    const sz: f64 = z / _scale;
+    const skew: f64 = (sx + sy + sz) * skew3d;
+    const ix: f64 = Math.floor(sx + skew);
+    const iy: f64 = Math.floor(sy + skew);
+    const iz: f64 = Math.floor(sz + skew);
+    const fx: f64 = sx + skew - ix;
+    const fy: f64 = sy + skew - iy;
+    const fz: f64 = sz + skew - iz;
+
+    return (
+      vertexContribution3d(ix, iy, iz, fx, fy, fz, 0, 0, 0) +
+      vertexContribution3d(ix, iy, iz, fx, fy, fz, 1, 0, 0) +
+      vertexContribution3d(ix, iy, iz, fx, fy, fz, 0, 1, 0) +
+      vertexContribution3d(ix, iy, iz, fx, fy, fz, 1, 1, 0) +
+      vertexContribution3d(ix, iy, iz, fx, fy, fz, 0, 0, 1) +
+      vertexContribution3d(ix, iy, iz, fx, fy, fz, 1, 0, 1) +
+      vertexContribution3d(ix, iy, iz, fx, fy, fz, 0, 1, 1) +
+      vertexContribution3d(ix, iy, iz, fx, fy, fz, 1, 1, 1) +
+      0.5
+    );
+  };
+};
+
+function vertexContribution3d(
+  ix: f64,
+  iy: f64,
+  iz: f64,
+  fx: f64,
+  fy: f64,
+  fz: f64,
+  cx: f64,
+  cy: f64,
+  cz: f64
+): f64 {
+  const dx: f64 = fx - cx;
+  const dy: f64 = fy - cy;
+  const dz: f64 = fz - cz;
+  const skewedOffset: f64 = (dx + dy + dz) * unskew3d;
+  const dxs: f64 = dx - skewedOffset;
+  const dys: f64 = dy - skewedOffset;
+  const dzs: f64 = dz - skewedOffset;
+
+  const a = rSquared3d - dxs * dxs - dys * dys - dzs * dzs;
+  if (a < 0) return 0;
+
+  // const h = <u8>(<i64>(noise(ix + cx, iy + cy, 0, 0) * 0xff));
+  // const u = <f64>(<i64>(<i8>(h & 0xf) - 8));
+  // const v = <f64>(<i64>(<i8>(h >> 4) - 8));
+  // return a * a * a * a * (u * dxs + v * dys);
+  const h = <u8>(<i64>(noise(ix + cx, iy + cy, iz + cz, 0) * 0xfff));
+  const u = <f64>(<i64>(<i8>(h & 0xf) - 8));
+  const v = <f64>(<i64>(((<i8>(h >> 4)) & 0xf) - 8));
+  const w = <f64>(<i64>(((<i8>(h >> 8)) & 0xf) - 8));
+  return (a * a * a * a * (u * dxs + v * dys + w * dzs)) / 2;
 }
--- src/as/assembly/world.ts
+++ src/as/assembly/world.ts
@@ -1,15 +1,17 @@
-import { OpenSimplex2dGenerator, makeOpenSimplex2dGenerator } from "./openSimplex";
+import { OpenSimplex2dGenerator, OpenSimplex3dGenerator, makeOpenSimplex2dGenerator, makeOpenSimplex3dGenerator } from "./openSimplex";
 import { NoiseGenerator, makeNoiseGenerator } from "./prng";
 
 export type Fn = (x: f64, y: f64, z: f64) => u8;
 
 let noise!: NoiseGenerator;
 let noise2!: OpenSimplex2dGenerator;
+let noise3!: OpenSimplex3dGenerator;
 export const makeWorldGenerator = (seed: i64): Fn => {
   noise = makeNoiseGenerator(seed);
   noise2 = makeOpenSimplex2dGenerator(seed);
+  noise3 = makeOpenSimplex3dGenerator(seed);
   return (x: f64, y: f64, z: f64): u8 => {
     // return <u8><i64>(noise(x, y, z, 0) * 0xff)
-    return <u8><i64>(noise2(x, y, z) * 0xff)
+    return <u8><i64>(noise3(x, y, z) * 0xff)
   };
 };

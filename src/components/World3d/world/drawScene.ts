7a9a26a028e79077e47135e78b85da52ad63e8bb
areve <areve@challen.info>
2024-09-04 23:11:39 +0100

    tidy

--- src/components/World3d/world/drawScene.ts
+++ src/components/World3d/world/drawScene.ts
@@ -1,11 +1,6 @@
 import { mat4 } from "gl-matrix";
 import { ProgramInfo } from "./program";
 import { Model } from "./landscapeModel";
-import {
-  setColorsAttribute,
-  setNormalsAttribute,
-  setPositionsAttribute,
-} from "./buffers";
 
 const vtl = -40;
 export function drawScene(
@@ -19,22 +14,19 @@ export function drawScene(
   landscapeModel: Model
 ) {
   initialize(gl);
-
   setPositionsAttribute(gl, buffers.positions, programInfo.vertexPosition);
   setColorsAttribute(gl, buffers.colors, programInfo.vertexColor);
   setNormalsAttribute(gl, buffers.normals, programInfo.vertexNormal);
 
   gl.useProgram(programInfo.program);
 
-  const modelViewMatrix = applyModelViewMatrix2(
-    gl,
-    programInfo.modelViewMatrix,
-    landscapeModel.width,
-    landscapeModel.height
-  );
+  const modelViewMatrix = createModelViewMatrix(landscapeModel.width);
+  const projectionMatrix = createProjectionMatrix(gl);
+  const normalMatrix = createNormalMatrix(gl, modelViewMatrix);
 
-  applyProjectionMatrix2(gl, programInfo.projectionMatrix);
-  doNormalMatrix(gl, modelViewMatrix, programInfo.normalMatrix);
+  gl.uniformMatrix4fv(programInfo.modelViewMatrix, false, modelViewMatrix);
+  gl.uniformMatrix4fv(programInfo.projectionMatrix, false, projectionMatrix);
+  gl.uniformMatrix4fv(programInfo.normalMatrix, false, normalMatrix);
 
   const vertexCount = landscapeModel.indices.length;
   draw(gl, vertexCount);
@@ -49,44 +41,94 @@ function initialize(gl: WebGLRenderingContext) {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
 
-function applyProjectionMatrix2(
+function setPositionsAttribute(
   gl: WebGLRenderingContext,
-  location: WebGLUniformLocation
+  buffer: WebGLBuffer,
+  vertexPosition: GLint
 ) {
+  const numComponents = 3;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(
+    vertexPosition,
+    numComponents,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+  gl.enableVertexAttribArray(vertexPosition);
+}
+
+function setColorsAttribute(
+  gl: WebGLRenderingContext,
+  buffer: WebGLBuffer,
+  vertexColor: GLint
+) {
+  const numComponents = 4;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(
+    vertexColor,
+    numComponents,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+  gl.enableVertexAttribArray(vertexColor);
+}
+
+function setNormalsAttribute(
+  gl: WebGLRenderingContext,
+  buffer: WebGLBuffer,
+  vertexNormal: GLint
+) {
+  const numComponents = 3;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(
+    vertexNormal,
+    numComponents,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+  gl.enableVertexAttribArray(vertexNormal);
+}
+
+function createProjectionMatrix(gl: WebGLRenderingContext) {
   const fieldOfView = (45 * Math.PI) / 180;
   const aspect = gl.canvas.width / gl.canvas.height;
   const zNear = 0.1;
   const zFar = 100.0;
   const matrix = mat4.create();
-
   mat4.perspective(matrix, fieldOfView, aspect, zNear, zFar);
-  gl.uniformMatrix4fv(location, false, matrix);
-}
-
-function applyModelViewMatrix2(
-  gl: WebGLRenderingContext,
-  location: WebGLUniformLocation,
-  width: number,
-  height: number
-) {
-  const matrix = mat4.create();
-  mat4.translate(matrix, matrix, [-width / 2, vtl, -50]);
-  mat4.rotate(matrix, matrix, -0.25 * Math.PI, [1, 0, 0]);
-
-  gl.uniformMatrix4fv(location, false, matrix);
   return matrix;
 }
 
-function doNormalMatrix(
-  gl: WebGLRenderingContext,
-  modelViewMatrix: mat4,
-  location: WebGLUniformLocation
-) {
-  const normalMatrix = mat4.create();
-  mat4.invert(normalMatrix, modelViewMatrix);
-  mat4.transpose(normalMatrix, normalMatrix);
+function createModelViewMatrix(width: number) {
+  const matrix = mat4.create();
+  mat4.translate(matrix, matrix, [-width / 2, vtl, -50]);
+  mat4.rotate(matrix, matrix, -0.25 * Math.PI, [1, 0, 0]);
+  return matrix;
+}
 
-  gl.uniformMatrix4fv(location, false, normalMatrix);
+function createNormalMatrix(gl: WebGLRenderingContext, modelViewMatrix: mat4) {
+  const matrix = mat4.create();
+  mat4.invert(matrix, modelViewMatrix);
+  mat4.transpose(matrix, matrix);
+  return matrix;
 }
 
 function draw(gl: WebGLRenderingContext, vertexCount: number) {

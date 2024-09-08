58f5f5c9c30d046b4069d292b5e0cab0136b8c18
areve <areve@challen.info>
2024-09-08 17:39:34 +0100

    Noise2 will be WebGPU

--- src/components/Noise2/Noise2.vue
+++ src/components/Noise2/Noise2.vue
@@ -1,16 +1,15 @@
-
 <template>
   <article>
     <h1>Noise2</h1>
     <p>
       Noise2 coming soon
+      <CanvasRenderer />
     </p>
-   
   </article>
 </template>
 
 <script lang="ts" setup>
+import CanvasRenderer from "./CanvasRenderer.vue";
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>

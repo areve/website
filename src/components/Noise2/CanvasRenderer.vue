80c462213da529a422c0a4e556b5d7b17cb0146b
areve <areve@challen.info>
2024-09-09 19:46:51 +0100

    a function

--- src/components/Noise2/CanvasRenderer.vue
+++ src/components/Noise2/CanvasRenderer.vue
@@ -10,10 +10,8 @@ const canvas = ref<HTMLCanvasElement>(undefined!);
 async function main() {
   const adapter = await navigator.gpu?.requestAdapter();
   const device = await adapter?.requestDevice()!;
-  if (!device) {
-    fail("need a browser that supports WebGPU");
-    return;
-  }
+  if (!device) return fail("need a browser that supports WebGPU");
+
   canvas.value.width = 500;
   canvas.value.height = 200;
 
@@ -24,30 +22,22 @@ async function main() {
     format: presentationFormat,
   });
 
-  // Pass canvas width and height to the GPU
-  const canvasWidth = canvas.value.width;
-  const canvasHeight = canvas.value.height;
-
+  console.clear();
   const module = device.createShaderModule({
-    label: "our hardcoded red line shaders",
+    label: "our hardcoded red color shader",
     code: `
       struct Uniforms {
           width: f32,
           height: f32,
           time: f32
       };
+
       @group(0) @binding(0) var<uniform> uUniforms: Uniforms;
 
-      // @vertex fn vs(
-      //   @builtin(vertex_index) vNdx : u32
-      // ) -> @builtin(position) vec4f {
-      //   let v = f32(vNdx) * 2.0 - 1.0;
-      //   let xy = vec2f(
-      //     v * cos(uUniforms.time),
-      //     v * sin(uUniforms.time),
-      //   );
-      //   return vec4f(xy, 0.0, 1.0);
-      // }
+      fn zzz() -> f32 {
+        return 0.5;
+      }
+
       @vertex fn vs(
         @builtin(vertex_index) vertexIndex : u32
       ) -> @builtin(position) vec4f {
@@ -64,8 +54,7 @@ async function main() {
       }
 
       @fragment fn fs(@builtin(position) coord: vec4<f32>) -> @location(0) vec4f {
-        //return vec4f(1, 0, 0, 1);
-        return vec4<f32>( coord.x / uUniforms.width * sin(uUniforms.time), coord.y / uUniforms.height * cos(uUniforms.time), cos(uUniforms.time  * 4), 1.0);
+        return vec4<f32>( coord.x / uUniforms.width * sin(uUniforms.time), zzz(), zzz(), 1.0);
       }
     `,
   });
@@ -80,9 +69,6 @@ async function main() {
       module,
       targets: [{ format: presentationFormat }],
     },
-    // primitive: {
-    //   topology: "line-list",
-    // },
   });
 
   const uniformValues = new Float32Array(3);
@@ -96,46 +82,32 @@ async function main() {
     entries: [{ binding: 0, resource: { buffer: uniformBuffer } }],
   });
 
-  const renderPassDescriptor = {
+  const colorAttachment: GPURenderPassColorAttachment = {
+    view: undefined! as GPUTextureView,
+    clearValue: [0.0, 0.0, 0.0, 1],
+    loadOp: "clear",
+    storeOp: "store",
+  };
+  const renderPassDescriptor: GPURenderPassDescriptor = {
     label: "our basic canvas renderPass",
-    colorAttachments: [
-      {
-        // view: <- to be filled out when we render
-        // view: context.getCurrentTexture().createView(),
-        clearValue: [0.3, 0.3, 0.3, 1],
-        loadOp: "clear",
-        storeOp: "store",
-      },
-    ],
+    colorAttachments: [colorAttachment],
   };
 
-  uniformValues[0] = canvasWidth;
-  uniformValues[1] = canvasHeight;
+  uniformValues[0] = canvas.value.width;
+  uniformValues[1] = canvas.value.height;
 
-  function render(time) {
-    // Update the uniforms.
+  function render(time: DOMHighResTimeStamp) {
     uniformValues[2] = time * 0.001;
     device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
-
-    // Get the current texture from the canvas context and
-    // set it as the texture to render to.
-    renderPassDescriptor.colorAttachments[0].view = context
-      .getCurrentTexture()
-      .createView();
-
-    // make a command encoder to start encoding commands
+    colorAttachment.view = context.getCurrentTexture().createView();
     const encoder = device.createCommandEncoder({ label: "our encoder" });
-
-    // make a render pass encoder to encode render specific commands
     const pass = encoder.beginRenderPass(renderPassDescriptor);
     pass.setPipeline(pipeline);
     pass.setBindGroup(0, bindGroup);
     pass.draw(6); // call our vertex shader 3 times.
     pass.end();
-
     const commandBuffer = encoder.finish();
     device.queue.submit([commandBuffer]);
-
     requestAnimationFrame(render);
   }
 

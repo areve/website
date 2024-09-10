4d62515809f20d9168fb81bb1df95d3d3c4901f2
areve <areve@challen.info>
2024-09-10 17:04:36 +0100

    smooth pan and zoon

--- src/components/Noise2/lib/controller.ts
+++ src/components/Noise2/lib/controller.ts
@@ -1,21 +1,29 @@
-import { mount } from "@vue/test-utils";
 import { ref } from "vue";
 
 export const makeController = function () {
-  let _tick = 0;
+  let moveSpeedX = 0;
+  let moveSpeedY = 0;
+  let zoomSpeed = 0;
   let _element: HTMLElement;
+  const start = performance.now() / 1000;
+  let prevTime = start;
   const controller = ref({
     mount(element?: HTMLElement) {
       _element = element ?? document.body;
       _element.addEventListener("keydown", onKeydown);
+      _element.addEventListener("keyup", onKeyup);
     },
     unmount(element?: HTMLElement) {
       _element.removeEventListener("keydown", onKeydown);
+      _element.removeEventListener("keyup", onKeyup);
     },
     update() {
-      // console.log("update controller");
-      // _tick++;
-      // this.x = _tick;
+      const now = performance.now() / 1000;
+      const diffTime = now - prevTime;
+      controller.value.x += moveSpeedX * diffTime;
+      controller.value.y += moveSpeedY * diffTime;
+      controller.value.zoom *= 1 - zoomSpeed * diffTime;
+      prevTime = now;
     },
     x: 0,
     y: 0,
@@ -29,11 +37,21 @@ export const makeController = function () {
 
   function onKeydown(event: KeyboardEvent) {
     const key = event.key.toLowerCase();
-    if (key === "a") controller.value.x -= 10;
-    if (key === "d") controller.value.x += 10;
-    if (key === "w") controller.value.y -= 10;
-    if (key === "s") controller.value.y += 10;
-    if (key === "'") controller.value.zoom /= 1.2;
-    if (key === "/") controller.value.zoom *= 1.2;
+    if (key === "a") moveSpeedX = -100;
+    if (key === "d") moveSpeedX = 100;
+    if (key === "w") moveSpeedY = -100;
+    if (key === "s") moveSpeedY = 100;
+    if (key === "'") zoomSpeed = 1;
+    if (key === "/") zoomSpeed = -1;
+  }
+
+  function onKeyup(event: KeyboardEvent) {
+    const key = event.key.toLowerCase();
+    if (key === "a") moveSpeedX = 0;
+    if (key === "d") moveSpeedX = 0;
+    if (key === "w") moveSpeedY = 0;
+    if (key === "s") moveSpeedY = 0;
+    if (key === "'") zoomSpeed = 0;
+    if (key === "/") zoomSpeed = 0;
   }
 };

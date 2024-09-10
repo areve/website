e6bc74843b03f0d2318a6e94fa6dc71da59ce257
areve <areve@challen.info>
2024-09-10 18:34:16 +0100

    tidy

--- src/components/Noise2/lib/controller.ts
+++ src/components/Noise2/lib/controller.ts
@@ -2,16 +2,16 @@ import { ref } from "vue";
 
 export const makeController = function () {
   let moveSpeedX = 0;
-  let maxSpeedX = 300;
-  const acceleration = 1000;
-  const deceleration = 2000;
   let moveSpeedY = 0;
-  let maxSpeedY = 300;
   let zoomSpeed = 0;
-  let maxZoomSpeed = 2;
-  const zoomAcceleration = 5;
+  const maxSpeedX = 300;
+  const maxSpeedY = 300;
+  const maxZoomSpeed = 2;
+  const acceleration = 2000;
+  const deceleration = 2000;
+  const zoomAcceleration = 20;
   const zoomDeceleration = 20;
-  
+
   const buttons = {
     left: false,
     right: false,
@@ -38,42 +38,38 @@ export const makeController = function () {
       const now = performance.now() / 1000;
       const diffTime = now - prevTime;
 
-      if (buttons.left === buttons.right) {
-        if (moveSpeedX > 0)
-          moveSpeedX = Math.max(moveSpeedX - deceleration * diffTime, 0);
-        else if (moveSpeedX < 0)
-          moveSpeedX = Math.min(moveSpeedX + deceleration * diffTime, 0);
-      } else if (buttons.left) {
-        moveSpeedX = Math.max(moveSpeedX - acceleration * diffTime, -maxSpeedX);
-      } else if (buttons.right) {
-        moveSpeedX = Math.min(moveSpeedX + acceleration * diffTime, maxSpeedX);
-      }
-
-      if (buttons.up === buttons.down) {
-        if (moveSpeedY > 0)
-          moveSpeedY = Math.max(moveSpeedY - deceleration * diffTime, 0);
-        else if (moveSpeedY < 0)
-          moveSpeedY = Math.min(moveSpeedY + deceleration * diffTime, 0);
-      } else if (buttons.down) {
-        moveSpeedY = Math.max(moveSpeedY - acceleration * diffTime, -maxSpeedY);
-      } else if (buttons.up) {
-        moveSpeedY = Math.min(moveSpeedY + acceleration * diffTime, maxSpeedY);
-      }
-
-      if (buttons.zoomIn === buttons.zoomOut) {
-        if (zoomSpeed > 0)
-          zoomSpeed = Math.max(zoomSpeed - zoomDeceleration * diffTime, 0);
-        else if (zoomSpeed < 0)
-          zoomSpeed = Math.min(zoomSpeed + zoomDeceleration * diffTime, 0);
-      } else if (buttons.zoomIn) {
-        zoomSpeed = Math.max(zoomSpeed - zoomAcceleration * diffTime, -maxZoomSpeed);
-      } else if (buttons.zoomOut) {
-        zoomSpeed = Math.min(zoomSpeed + zoomAcceleration * diffTime, maxZoomSpeed);
-      }
+      moveSpeedX = updateSpeed(
+        moveSpeedX,
+        buttons.left,
+        buttons.right,
+        acceleration,
+        deceleration,
+        maxSpeedX,
+        diffTime
+      );
+      moveSpeedY = updateSpeed(
+        moveSpeedY,
+        buttons.up,
+        buttons.down,
+        acceleration,
+        deceleration,
+        maxSpeedY,
+        diffTime
+      );
+      zoomSpeed = updateSpeed(
+        zoomSpeed,
+        buttons.zoomIn,
+        buttons.zoomOut,
+        zoomAcceleration,
+        zoomDeceleration,
+        maxZoomSpeed,
+        diffTime
+      );
 
       controller.value.x += moveSpeedX * diffTime;
       controller.value.y += moveSpeedY * diffTime;
-      controller.value.zoom *= 1 + zoomSpeed * diffTime;
+      controller.value.zoom *= 1 - zoomSpeed * diffTime;
+
       prevTime = now;
     },
     x: 0,
@@ -85,6 +81,29 @@ export const makeController = function () {
     zoom: 1,
   });
   return controller;
+
+  function updateSpeed(
+    speed: number,
+    positiveButton: boolean,
+    negativeButton: boolean,
+    accel: number,
+    decel: number,
+    maxSpeed: number,
+    diffTime: number
+  ): number {
+    if (positiveButton === negativeButton) {
+      if (speed > 0) {
+        speed = Math.max(speed - decel * diffTime, 0);
+      } else if (speed < 0) {
+        speed = Math.min(speed + decel * diffTime, 0);
+      }
+    } else if (positiveButton) {
+      speed = Math.min(speed + accel * diffTime, maxSpeed);
+    } else if (negativeButton) {
+      speed = Math.max(speed - accel * diffTime, -maxSpeed);
+    }
+    return speed;
+  }
 
   function onKeydown(event: KeyboardEvent) {
     const key = event.key.toLowerCase();

9a80bd556bfeaaf585fbb9aac596bce83c15b363
areve <areve@challen.info>
2024-09-10 20:35:33 +0100

    pinch zoom

--- src/components/Noise2/lib/controller.ts
+++ src/components/Noise2/lib/controller.ts
@@ -31,13 +31,23 @@ export const makeController = function () {
       moveY: { increasing: false, decreasing: false, speed: 0 },
       zoom: { increasing: false, decreasing: false, speed: 0 },
     },
-    pointer: {
+    mouse: {
       startX: 0,
       startY: 0,
       dragging: false,
       currentX: 0,
       currentY: 0,
     },
+    touch: {
+      startX: 0,
+      startY: 0,
+      currentX: 0,
+      currentY: 0,
+      startDistance: 0,
+      zooming: false,
+      dragging: false,
+    },
+    initialPinchDistance: 0,
   };
 
   let bindElement: HTMLElement;
@@ -50,18 +60,24 @@ export const makeController = function () {
       bindElement = element ?? document.body;
       bindGlobalElement.addEventListener("keydown", onKeyDown);
       bindGlobalElement.addEventListener("keyup", onKeyUp);
-      bindElement.addEventListener("pointerdown", onPointerDown);
-      bindElement.addEventListener("pointermove", onPointerMove);
-      bindElement.addEventListener("pointerup", onPointerEnd);
-      bindElement.addEventListener("wheel", onWheel); // Add this line
+      bindElement.addEventListener("mousedown", onMouseDown);
+      bindElement.addEventListener("mousemove", onMouseMove);
+      bindElement.addEventListener("mouseup", onMouseUp);
+      bindElement.addEventListener("wheel", onWheel);
+      bindElement.addEventListener("touchstart", onTouchStart);
+      bindElement.addEventListener("touchmove", onTouchMove);
+      bindElement.addEventListener("touchend", onTouchEnd);
     },
     unmount() {
       bindGlobalElement.removeEventListener("keydown", onKeyDown);
       bindGlobalElement.removeEventListener("keyup", onKeyUp);
-      bindElement.removeEventListener("pointerdown", onPointerDown);
-      bindElement.removeEventListener("pointermove", onPointerMove);
-      bindElement.removeEventListener("pointerup", onPointerEnd);
-      bindElement.removeEventListener("wheel", onWheel); // Add this line
+      bindElement.removeEventListener("mousedown", onMouseDown);
+      bindElement.removeEventListener("mousemove", onMouseMove);
+      bindElement.removeEventListener("mouseup", onMouseUp);
+      bindElement.removeEventListener("wheel", onWheel);
+      bindElement.removeEventListener("touchstart", onTouchStart);
+      bindElement.removeEventListener("touchmove", onTouchMove);
+      bindElement.removeEventListener("touchend", onTouchEnd);
     },
     update() {
       const now = performance.now() / 1000;
@@ -87,17 +103,27 @@ export const makeController = function () {
       controller.value.y -= states.buttons.moveY.speed * diffTime;
       controller.value.zoom *= 1 - states.buttons.zoom.speed * diffTime;
 
-      if (states.pointer.dragging) {
+      if (states.mouse.dragging) {
         const deltaX =
-          (states.pointer.startX - states.pointer.currentX) *
-          controller.value.zoom;
+          (states.mouse.startX - states.mouse.currentX) * controller.value.zoom;
         const deltaY =
-          (states.pointer.startY - states.pointer.currentY) *
-          controller.value.zoom;
+          (states.mouse.startY - states.mouse.currentY) * controller.value.zoom;
         controller.value.x += deltaX;
         controller.value.y += deltaY;
-        states.pointer.startX = states.pointer.currentX;
-        states.pointer.startY = states.pointer.currentY;
+        states.mouse.startX = states.mouse.currentX;
+        states.mouse.startY = states.mouse.currentY;
+      }
+
+      if (states.touch.zooming) {
+        const distance = Math.sqrt(
+          Math.pow(states.touch.currentX - states.touch.startX, 2) +
+            Math.pow(states.touch.currentY - states.touch.startY, 2)
+        );
+        const scale = distance / states.touch.startDistance;
+        controller.value.zoom *= scale;
+        states.touch.startDistance = distance;
+        states.touch.startX = states.touch.currentX;
+        states.touch.startY = states.touch.currentY;
       }
 
       prevTime = now;
@@ -142,23 +168,60 @@ export const makeController = function () {
     }
   }
 
-  function onPointerDown(event: PointerEvent) {
-    states.pointer.currentX = states.pointer.startX = event.clientX;
-    states.pointer.currentY = states.pointer.startY = event.clientY;
-    states.pointer.dragging = true;
+  function onMouseDown(event: MouseEvent) {
+    states.mouse.startX = event.clientX;
+    states.mouse.startY = event.clientY;
+    states.mouse.dragging = true;
     event.preventDefault();
   }
 
-  function onPointerMove(event: PointerEvent) {
-    if (states.pointer.dragging) {
-      states.pointer.currentX = event.clientX;
-      states.pointer.currentY = event.clientY;
+  function onMouseMove(event: MouseEvent) {
+    if (states.mouse.dragging) {
+      states.mouse.currentX = event.clientX;
+      states.mouse.currentY = event.clientY;
       event.preventDefault();
     }
   }
 
-  function onPointerEnd() {
-    states.pointer.dragging = false;
+  function onMouseUp() {
+    states.mouse.dragging = false;
+  }
+
+  function onTouchStart(event: TouchEvent) {
+    if (event.touches.length === 1) {
+      states.touch.startX = event.touches[0].clientX;
+      states.touch.startY = event.touches[0].clientY;
+      states.touch.dragging = true;
+    } else if (event.touches.length === 2) {
+      const [touch1, touch2] = event.touches as unknown as [Touch, Touch];
+      states.initialPinchDistance = getDistance(touch1, touch2);
+      states.touch.dragging = false; // Disable dragging during pinch
+    }
+    event.preventDefault();
+  }
+
+  function onTouchMove(event: TouchEvent) {
+    if (event.touches.length === 1 && states.touch.dragging) {
+      states.touch.currentX = event.touches[0].clientX;
+      states.touch.currentY = event.touches[0].clientY;
+      event.preventDefault();
+    } else if (event.touches.length === 2) {
+      const [touch1, touch2] = event.touches as unknown as [Touch, Touch];
+      const currentPinchDistance = getDistance(touch1, touch2);
+      const pinchRatio = states.initialPinchDistance / currentPinchDistance; // Invert the ratio
+      controller.value.zoom *= pinchRatio;
+      states.initialPinchDistance = currentPinchDistance; // Update for continuous zooming
+      event.preventDefault();
+    }
+  }
+
+  function onTouchEnd(event: TouchEvent) {
+    if (event.touches.length === 0) {
+      states.touch.dragging = false;
+    } else if (event.touches.length === 1) {
+      states.touch.dragging = true; // Resume dragging if one finger is left
+    }
+    event.preventDefault();
   }
 
   function onWheel(event: WheelEvent) {
@@ -169,5 +232,12 @@ export const makeController = function () {
     );
 
     event.preventDefault();
+  }
+
+  function getDistance(touch1: Touch, touch2: Touch): number {
+    return Math.sqrt(
+      Math.pow(touch2.clientX - touch1.clientX, 2) +
+        Math.pow(touch2.clientY - touch1.clientY, 2)
+    );
   }
 };

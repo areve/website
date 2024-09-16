3dd7c3f056c13a5410c8dc659e1ca84ec0a1c700
areve <areve@challen.info>
2024-09-16 21:15:02 +0100

    tidy

--- src/components/Noise2/Noise2.vue
+++ src/components/Noise2/Noise2.vue
@@ -2,7 +2,8 @@
   <article>
     <h1>Noise2</h1>
     <p>This page doesn't work in all browsers, but its using WebGPU to render noise on the GPU, much faster than all my previous efforts.</p>
-    <!-- <p>
+    <!-- 
+    <p>
       <div>WebGPUNoise</div>
       <WebGPUNoise />
     </p>

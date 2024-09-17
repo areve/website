be21b620a75a3db5a70caef55bf0ed36a34d806b
areve <areve@challen.info>
2024-09-17 08:57:40 +0100

    moved setupWorldRenderer

--- src/components/Noise2/WebGPUWorld3d/WebGPUWorld3d.vue
+++ src/components/Noise2/WebGPUWorld3d/WebGPUWorld3d.vue
@@ -9,7 +9,11 @@
 import { onMounted, onUnmounted, ref } from "vue";
 import { makeStats } from "../lib/stats";
 import { makeController } from "../lib/controller";
-import { setupWorldRenderer } from "./lib/setupWorldRenderer";
+import { vec3 } from "wgpu-matrix";
+import { createRenderer, getDeviceContext } from "./lib/webgpu";
+import { createCube } from "./models/createCube";
+import { createPlane } from "./models/createPlane";
+import { createCamera } from "./lib/camera";
 
 const canvas = ref<HTMLCanvasElement>(undefined!);
 const stats = makeStats();
@@ -46,6 +50,86 @@ onUnmounted(() => {
   cancelAnimationFrame(frameId);
   controller.value.unmount();
 });
+
+async function setupWorldRenderer(
+  canvas: HTMLCanvasElement,
+  options: {
+    width: number;
+    height: number;
+    seed?: number;
+    scale?: number;
+  }
+) {
+  const { device, context } = await getDeviceContext(
+    canvas,
+    options.width,
+    options.height
+  );
+
+  const worldMapUniforms = {
+    width: options.width,
+    height: options.height,
+    seed: options.seed ?? 12345,
+    scale: options.scale ?? 1,
+    x: 0,
+    y: 0,
+    z: 0,
+    zoom: 1,
+    toBuffer() {
+      return new Float32Array([
+        this.width,
+        this.height,
+        this.seed,
+        this.scale,
+        this.x,
+        this.y,
+        this.z,
+        this.zoom,
+      ]);
+    },
+  };
+
+  const camera = createCamera(options.width, options.height);
+
+  const cube = createCube(
+    device,
+    () => worldMapUniforms.toBuffer(),
+    () => camera
+  );
+
+  const plane = createPlane(
+    device,
+    () => worldMapUniforms.toBuffer(),
+    () => camera
+  );
+
+  const renderer = createRenderer(device, options.width, options.height);
+
+  return {
+    async init() {},
+    async update(
+      time: DOMHighResTimeStamp,
+      data?: {
+        x?: number;
+        y?: number;
+      }
+    ) {
+      const t = time * 0.001;
+      if (data) Object.assign(worldMapUniforms, data);
+      // worldMapUniforms.z = t;
+      cube.transform.rotation = vec3.create(Math.sin(t), Math.cos(t), 0);
+
+      // cube.updateBuffers();
+      plane.updateBuffers();
+
+      const renderPass = renderer.start(context);
+      // cube.render(renderPass);
+      plane.render(renderPass);
+      return renderer.end();
+    },
+  };
+}
+
 </script>
 
 <style scoped></style>

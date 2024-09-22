dfe000564702aa6e08c02d15b039c78d1c22ed90
areve <areve@challen.info>
2024-09-22 15:59:23 +0100

    opensimplex is back

--- src/components/World2/WebGPUWorld3d/models/createPlane.ts
+++ src/components/World2/WebGPUWorld3d/models/createPlane.ts
@@ -183,6 +183,57 @@ export function createPlane(
           @group(0) @binding(0) 
           var<storage, read_write> textureData: array<vec4f>; 
       
+          fn noise(seed: f32, coord: vec4<f32>) -> f32 {
+            let n: u32 = bitcast<u32>(seed) + bitcast<u32>(coord.x * 374761393.0) + bitcast<u32>(coord.y * 668265263.0) + bitcast<u32>(coord.z * 1440662683.0) + bitcast<u32>(coord.w * 3865785317.0);
+            let m: u32 = (n ^ (n >> 13)) * 1274126177;
+            return f32(m) / f32(0xffffffff);
+          }
+    
+          const skew3d: f32 = 1.0 / 3.0;
+          const unskew3d: f32 = 1.0 / 6.0;
+          const rSquared3d: f32 = 3.0 / 4.0;
+
+          fn openSimplex3d(seed: f32, x: f32, y: f32, z: f32) -> f32 {
+              let sx: f32 = x;
+              let sy: f32 = y;
+              let sz: f32 = z;
+              let skew: f32 = (sx + sy + sz) * skew3d;
+              let ix: i32 = i32(floor(sx + skew));
+              let iy: i32 = i32(floor(sy + skew));
+              let iz: i32 = i32(floor(sz + skew));
+              let fx: f32 = sx + skew - f32(ix);
+              let fy: f32 = sy + skew - f32(iy);
+              let fz: f32 = sz + skew - f32(iz);
+
+              return 0.5 + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 0, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 0, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 1, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 1, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 0, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 0, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 1, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 1, 1) ;
+          }
+
+          fn vertexContribution(
+              seed: f32,
+              ix: i32, iy: i32, iz: i32,
+              fx: f32, fy: f32, fz: f32,
+              cx: i32, cy: i32, cz: i32
+          ) -> f32 {
+              let dx: f32 = fx - f32(cx);
+              let dy: f32 = fy - f32(cy);
+              let dz: f32 = fz - f32(cz);
+              let skewedOffset: f32 = (dx + dy + dz) * unskew3d;
+              let dxs: f32 = dx - skewedOffset;
+              let dys: f32 = dy - skewedOffset;
+              let dzs: f32 = dz - skewedOffset;
+
+              let a: f32 = rSquared3d - dxs * dxs - dys * dys - dzs * dzs;
+              if a < 0.0 {
+                  return 0.0;
+              }
+
+              let h: i32 = bitcast<i32>(noise(seed, vec4f(f32(ix + cx), f32(iy + cy), f32(iz + cz), 0.0))) & 0xfff;
+              let u: i32 = (h & 0xf) - 8;
+              let v: i32 = ((h >> 4) & 0xf) - 8;
+              let w: i32 = ((h >> 8) & 0xf) - 8;
+              return (a * a * a * a * (f32(u) * dxs + f32(v) * dys + f32(w) * dzs)) / 2.0;
+          }
+
           @compute @workgroup_size(16, 16)
           fn computeMain(@builtin(global_invocation_id) global_id: vec3<u32>) {
               let x = global_id.x;
@@ -190,11 +241,12 @@ export function createPlane(
               let index = y * 500u + x;
               if (x < 500u && y < 500u) {
                 let index = y * 500u + x;
-        
+                
+                let n = openSimplex3d(12345.0, f32(x) / 64.0, f32(y) / 64.0, 0.0);
                 textureData[index] = vec4f(
-                  f32(x) / 500.0,
-                  f32(y) / 500.0,
-                  0.5,
+                  n,
+                  n,//f32(y) / 500.0,
+                  n, //0.5,
                   1.0
                 );
   

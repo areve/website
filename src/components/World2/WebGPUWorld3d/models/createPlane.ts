4ef95942fcd7a5064869a092d460568c6f814b92
areve <areve@challen.info>
2024-09-22 18:23:47 +0100

    worldPoint struct

--- src/components/World2/WebGPUWorld3d/models/createPlane.ts
+++ src/components/World2/WebGPUWorld3d/models/createPlane.ts
@@ -42,14 +42,14 @@ export function createPlane(
 
   const worldWgsl = /* wgsl */ `
     struct Uniforms {
-        width: f32,
-        height: f32,
-        seed: f32,
-        scale: f32,
-        x: f32,
-        y: f32,
-        z: f32,
-        zoom: f32
+      width: f32,
+      height: f32,
+      seed: f32,
+      scale: f32,
+      x: f32,
+      y: f32,
+      z: f32,
+      zoom: f32
     };
 
     struct VertexOutput {
@@ -64,6 +64,14 @@ export function createPlane(
       transform: mat4x4f
     };
 
+    struct WorldPoint {
+      height: f32,
+      temperature: f32,
+      moisture: f32,
+      iciness: f32,
+      desert: f32
+    };
+
     @group(0) @binding(0)
     var<uniform> uniforms: Uniforms;
 
@@ -71,7 +79,7 @@ export function createPlane(
     var<uniform> uniforms2: Uniforms2;
 
     @group(2) @binding(0) 
-    var<storage> textureData: array<vec4f>; 
+    var<storage> textureData: array<WorldPoint>; 
 
     @vertex
     fn vertexMain(
@@ -88,12 +96,12 @@ export function createPlane(
         );
 
         let index = u32(uv.y * 500) * 500+ u32(uv.x * 500);
-        let foo = textureData[index];
+        let worldPoint = textureData[index];
 
         var output: VertexOutput;
-        output.position = uniforms2.transform * (position + vec4f(0.0, 0.0, foo.z, 0.0));
+        output.position = uniforms2.transform * (position + vec4f(0.0, 0.0, worldPoint.height, 0.0));
         output.uv = uv;
-        output.color = vec4f(foo.x, 1.0, 0.0, 1.0);
+        output.color = vec4f(worldPoint.height, 1.0, 0.0, 1.0);
         return output;
     }
 
@@ -106,8 +114,8 @@ export function createPlane(
     ) -> @location(0) vec4f {
         let dummy = uniforms.seed;
         let index = u32(uv.y * 500) * 500+ u32(uv.x * 500);
-        let foo = textureData[index];
-        return vec4f(color.x, foo.y, foo.z, 1.0);
+        let worldPoint = textureData[index];
+        return vec4f(color.x, worldPoint.temperature, worldPoint.moisture, 1.0);
     }
   `;
 
@@ -183,9 +191,70 @@ export function createPlane(
     compute: {
       module: device.createShaderModule({
         code: /* wgsl */ `
+          // let height = worldPointHeight(f32(x), f32(y), 0.0);
+          // let temperature = worldPointTemperature(f32(x), f32(y), 0.0);
+          // let moisture = worldPointMoisture(f32(x), f32(y), 0.0);
+
+          // let iciness = c(heightIcinessCurve(height) + temperatureIcinessCurve(temperature));
+          // let desert = c(moistureDesertCurve(moisture) + temperatureDesertCurve(temperature));
+
+          struct WorldPoint {
+            height: f32,
+            temperature: f32,
+            moisture: f32,
+            iciness: f32,
+            desert: f32
+          };
+    
           @group(0) @binding(0) 
-          var<storage, read_write> textureData: array<vec4f>; 
+          var<storage, read_write> textureData: array<WorldPoint>; 
       
+          fn clamp(value: f32, low: f32, high: f32) -> f32 {
+            return min(max(value, low), high);
+          }
+      
+          fn c(v: f32) -> f32 {
+            return clamp(v, 0, 1);
+          }
+
+          fn piecewiseCurve(t: f32, p: f32, s: f32) -> f32 {
+            var c: f32;
+            if s == 3.0 {
+                c = 1e10;
+            } else {
+                c = (1.0 - s) / (s - 3.0);
+            }
+    
+            if t < p {
+              let n = t * (1.0 + c);
+              let d = t + p * c;
+              let r = n / d;
+              return t * r * r;
+            } else {
+              let v = 1.0 - t;
+              let n = v * (1.0 + c);
+              let d = v + (1.0 - p) * c;
+              let r = n / d;
+              return 1.0 - v * r * r;
+            }
+          }
+
+          fn heightIcinessCurve(t: f32) -> f32 {
+            return piecewiseCurve(t, 0.8, 15.0);
+          }
+                    
+          fn temperatureIcinessCurve(t: f32) -> f32 {
+            return 1 - piecewiseCurve(t, 0.3, 6.0);
+          }
+      
+          fn moistureDesertCurve(t: f32) -> f32 {
+            return 1 - piecewiseCurve(t, 0.3, 10.0);
+          }
+                    
+          fn temperatureDesertCurve(t: f32) -> f32 {
+            return piecewiseCurve(t, 0.7, 8.0);
+          }
+    
           fn noise(seed: f32, coord: vec4<f32>) -> f32 {
             let n: u32 = bitcast<u32>(seed) + bitcast<u32>(coord.x * 374761393.0) + bitcast<u32>(coord.y * 668265263.0) + bitcast<u32>(coord.z * 1440662683.0) + bitcast<u32>(coord.w * 3865785317.0);
             let m: u32 = (n ^ (n >> 13)) * 1274126177;
@@ -197,44 +266,44 @@ export function createPlane(
           const rSquared3d: f32 = 3.0 / 4.0;
 
           fn openSimplex3d(seed: f32, x: f32, y: f32, z: f32) -> f32 {
-              let sx: f32 = x;
-              let sy: f32 = y;
-              let sz: f32 = z;
-              let skew: f32 = (sx + sy + sz) * skew3d;
-              let ix: i32 = i32(floor(sx + skew));
-              let iy: i32 = i32(floor(sy + skew));
-              let iz: i32 = i32(floor(sz + skew));
-              let fx: f32 = sx + skew - f32(ix);
-              let fy: f32 = sy + skew - f32(iy);
-              let fz: f32 = sz + skew - f32(iz);
+            let sx: f32 = x;
+            let sy: f32 = y;
+            let sz: f32 = z;
+            let skew: f32 = (sx + sy + sz) * skew3d;
+            let ix: i32 = i32(floor(sx + skew));
+            let iy: i32 = i32(floor(sy + skew));
+            let iz: i32 = i32(floor(sz + skew));
+            let fx: f32 = sx + skew - f32(ix);
+            let fy: f32 = sy + skew - f32(iy);
+            let fz: f32 = sz + skew - f32(iz);
 
-              return 0.5 + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 0, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 0, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 1, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 1, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 0, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 0, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 1, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 1, 1) ;
+            return 0.5 + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 0, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 0, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 1, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 1, 0) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 0, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 0, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 0, 1, 1) + vertexContribution(seed, ix, iy, iz, fx, fy, fz, 1, 1, 1) ;
           }
 
           fn vertexContribution(
-              seed: f32,
-              ix: i32, iy: i32, iz: i32,
-              fx: f32, fy: f32, fz: f32,
-              cx: i32, cy: i32, cz: i32
+            seed: f32,
+            ix: i32, iy: i32, iz: i32,
+            fx: f32, fy: f32, fz: f32,
+            cx: i32, cy: i32, cz: i32
           ) -> f32 {
-              let dx: f32 = fx - f32(cx);
-              let dy: f32 = fy - f32(cy);
-              let dz: f32 = fz - f32(cz);
-              let skewedOffset: f32 = (dx + dy + dz) * unskew3d;
-              let dxs: f32 = dx - skewedOffset;
-              let dys: f32 = dy - skewedOffset;
-              let dzs: f32 = dz - skewedOffset;
+            let dx: f32 = fx - f32(cx);
+            let dy: f32 = fy - f32(cy);
+            let dz: f32 = fz - f32(cz);
+            let skewedOffset: f32 = (dx + dy + dz) * unskew3d;
+            let dxs: f32 = dx - skewedOffset;
+            let dys: f32 = dy - skewedOffset;
+            let dzs: f32 = dz - skewedOffset;
 
-              let a: f32 = rSquared3d - dxs * dxs - dys * dys - dzs * dzs;
-              if a < 0.0 {
-                  return 0.0;
-              }
+            let a: f32 = rSquared3d - dxs * dxs - dys * dys - dzs * dzs;
+            if a < 0.0 {
+              return 0.0;
+            }
 
-              let h: i32 = bitcast<i32>(noise(seed, vec4f(f32(ix + cx), f32(iy + cy), f32(iz + cz), 0.0))) & 0xfff;
-              let u: i32 = (h & 0xf) - 8;
-              let v: i32 = ((h >> 4) & 0xf) - 8;
-              let w: i32 = ((h >> 8) & 0xf) - 8;
-              return (a * a * a * a * (f32(u) * dxs + f32(v) * dys + f32(w) * dzs)) / 2.0;
+            let h: i32 = bitcast<i32>(noise(seed, vec4f(f32(ix + cx), f32(iy + cy), f32(iz + cz), 0.0))) & 0xfff;
+            let u: i32 = (h & 0xf) - 8;
+            let v: i32 = ((h >> 4) & 0xf) - 8;
+            let w: i32 = ((h >> 8) & 0xf) - 8;
+            return (a * a * a * a * (f32(u) * dxs + f32(v) * dys + f32(w) * dzs)) / 2.0;
           }
 
           fn worldPointHeight(x: f32, y:f32, z:f32) -> f32 {
@@ -259,23 +328,22 @@ export function createPlane(
 
           @compute @workgroup_size(16, 16)
           fn computeMain(@builtin(global_invocation_id) global_id: vec3<u32>) {
-              let x = global_id.x;
-              let y = global_id.y;
+            let x = global_id.x;
+            let y = global_id.y;
+            let index = y * 500u + x;
+            if (x < 500u && y < 500u) {
               let index = y * 500u + x;
-              if (x < 500u && y < 500u) {
-                let index = y * 500u + x;
-                
-                let n = worldPointHeight(f32(x), f32(y), 0.0);
-                let m = worldPointTemperature(f32(x), f32(y), 0.0);
-                let o = worldPointMoisture(f32(x), f32(y), 0.0);
-                textureData[index] = vec4f(
-                  n,
-                  m,//f32(y) / 500.0,
-                  o, //0.5,
-                  1.0
-                );
-  
-              }
+              
+              var worldPoint: WorldPoint;
+              worldPoint.height = worldPointHeight(f32(x), f32(y), 0.0);
+              worldPoint.temperature = worldPointTemperature(f32(x), f32(y), 0.0);
+              worldPoint.moisture = worldPointMoisture(f32(x), f32(y), 0.0);
+
+              worldPoint.iciness = c(heightIcinessCurve(worldPoint.height) + temperatureIcinessCurve(worldPoint.temperature));
+              worldPoint.desert = c(moistureDesertCurve(worldPoint.moisture) + temperatureDesertCurve(worldPoint.temperature));
+
+              textureData[index] = worldPoint;
+            }
           }
         `,
       }),

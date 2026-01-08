b378c6886aebeb03e7d3904be2b7393bc10babaa
areve <areve@challen.info>
2026-01-08 20:12:05 +0000

    added documentation

--- src/components/World2/World2.vue
+++ src/components/World2/World2.vue
@@ -1,7 +1,17 @@
 <template>
   <section>
     <h1>World</h1>
-    <p>A pseudo random number generated world map</p>
+    <p>
+      A pseudo random number generated world map.
+      Controls and keyboard shortcuts are listed below for quick reference.
+    </p>
+    <ul class="controls">
+      <li><kbd>Mouse</kbd>: drag to pan, scroll/wheel to zoom</li>
+      <li><kbd>W</kbd><kbd>A</kbd><kbd>S</kbd><kbd>D</kbd>: pan the view (A = left, D = right, W = up, S = down)</li>
+      <li><kbd>'</kbd> (apostrophe) / <kbd>/</kbd> : zoom in / out (keyboard accelerator)</li>
+      <li><kbd>Space</kbd> or <kbd>P</kbd>: pause / resume animation; double-click/tap also toggles pause</li>
+      <li>Touch: two-finger pinch to zoom, drag to pan</li>
+    </ul>
     <div class="panels">
     
       <div class="panel">
